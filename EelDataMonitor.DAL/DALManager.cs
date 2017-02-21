2332037f197839c93ddf4080508044899bc70482
dani288e <daniel_holm@live.dk>
2017-02-21 10:02:36 +0100

    initial commit

---
+++ EelDataMonitor.DAL/DALManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EelDataMonitor.DAL
+{
+    public class DALManager
+    {
+        public List<SensorData> GetSesnsorDatas(List<SensorData> sensorDatas)
+        {
+            using (Ringsted1Entities context = new Ringsted1Entities())
+            {
+                DateTime dateCriteria = DateTime.Now.AddDays(-30);
+
+                var query = (from o in context.SensorDatas
+                             select o).ToList();
+                if (query != null)
+                {
+                    foreach (var result in query)
+                    {
+                        sensorDatas.Add(result);
+                    }
+                }
+            }
+            return sensorDatas;
+        }
+    }
+}
---
+++ EeldataMonitor/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EeldataMonitor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGrid_SensorDatas_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Feed_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_CreateBassin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
